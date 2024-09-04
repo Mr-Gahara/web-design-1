d609ebab89992f2e0aa1e308c4ae905297f210d9
Mr-Gahara <zorrokeren3@gmail.com>
2024-09-04 18:58:55 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function scrollToHome() {
+    const homeSection = document.getElementById('home-section');
+    homeSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToPortfolio() {
+    const portfolioSection = document.getElementById('portfolio-section');
+    portfolioSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToAbout() {
+    const aboutSection = document.getElementById('about-section');
+    aboutSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToContact() {
+    const contactSection = document.getElementById('contact-section');
+    contactSection.scrollIntoView({ behavior: 'smooth' });
+}
